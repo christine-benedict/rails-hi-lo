f0b5d3dfa0c7dc1489172f295f8f2209a5534ffa
learn <learn@learn08.localdomain>
2018-04-23 16:49:56 -0700

    spaces and other organization edits

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,12 +1,15 @@
 class GameController < ApplicationController
     def try
         @playername = params[:name]
+
         if @playername != nil
             cookies[:name] = @playername
         end
+
         if session[:number] == nil
             session[:number] = rand(1..100)
         end
+
         if session[:number].to_i > params[:number].to_i
             @result = "Too low"
         elsif session[:number].to_i < params[:number].to_i
@@ -15,13 +18,16 @@ class GameController < ApplicationController
             @result = "Correct!"
             session[:number] = nil
         end
+
         if session[:number] == nil
             cookies[:counter] = 0
         else
             cookies[:counter] = cookies[:counter].to_i + 1
         end
+
         render "try.html.erb"
     end
+    
     def reset
         session[:number] = nil
         cookies[:counter] = 0
